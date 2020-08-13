960d14655f391053f4e423fcae98acda3a80f285
Fernando <46842299+fersgva@users.noreply.github.com>
2020-08-13 16:46:48 +0200

    Water

--- Trapball2/Assets/Scripts/FloatingBehaviour.cs
+++ Trapball2/Assets/Scripts/FloatingBehaviour.cs
@@ -9,6 +9,7 @@ public class FloatingBehaviour : MonoBehaviour
     bool floating;
     Rigidbody rb;
     float waterYPos;
+    [SerializeField] float torque;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +23,22 @@ public class FloatingBehaviour : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if(floating)
+        float zRotation = transform.eulerAngles.z;
+        int turnDirection;
+        if (floating)
         {
             //Si la caja está por encima del agua, entonces la variable quedará como negativa y positiva al contrario
-            float displacementMultiplier = Mathf.Clamp01(waterYPos - transform.position.y / depthBeforeSumerged) * displacementAmount;
+            float displacementMultiplier = Mathf.Clamp01((waterYPos - transform.position.y) / depthBeforeSumerged) * displacementAmount;
             //Así, se va aplicando una fuerza en ambas direcciones (arriba y abajo) en función de la posición de la caja respecto al agua.
             //Finalmente, se acabará cancelando la fuerza aplicada ya que las posiciones se igualarán.
             rb.AddForce(new Vector3(0, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0), ForceMode.Acceleration);
+
+            if (zRotation > 0.2f && zRotation < 359.8f)
+            {
+                turnDirection = zRotation > 0.2f && zRotation < 180 ? -1 : 1;
+                rb.AddTorque(transform.forward * torque * turnDirection, ForceMode.Acceleration);
+            }
+            
 
         }
     }
@@ -36,8 +46,10 @@ public class FloatingBehaviour : MonoBehaviour
     {
         if(other.CompareTag("Water"))
         {
+            rb.mass = 10; //Para mejorar comportamiento cuando la bola se pone encima de una caja en el agua.
             waterYPos = other.transform.position.y;
-            rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+            //rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
             floating = true;
         }
     }
